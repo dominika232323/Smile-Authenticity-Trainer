ff13d1c14aa73f383de424b6d6ea52ebd5e12a26
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2025-10-13 13:36:22 +0200

    Extract main function

--- ai/data_preprocessing/main.py
+++ ai/data_preprocessing/main.py
@@ -2,7 +2,11 @@ from ai.data_preprocessing.get_details import get_details
 from ai.config import UvA_NEMO_SMILE_DETAILS, PREPROCESSED_UvA_NEMO_SMILE_DATABASE_DIR
 
 
-if __name__ == "__main__":
+def main():
     PREPROCESSED_UvA_NEMO_SMILE_DATABASE_DIR.mkdir(parents=True, exist_ok=True)
 
     get_details(UvA_NEMO_SMILE_DETAILS).to_csv(PREPROCESSED_UvA_NEMO_SMILE_DATABASE_DIR / "details.csv", index=False)
+
+
+if __name__ == "__main__":
+    main()
