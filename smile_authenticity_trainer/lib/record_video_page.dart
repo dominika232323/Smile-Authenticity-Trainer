b6b7e771a9c69855e3589eb9ec6c9936a2aae23a
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2025-12-29 02:15:29 +0100

    Add showing the video after picking

--- smile_authenticity_trainer/lib/record_video_page.dart
+++ smile_authenticity_trainer/lib/record_video_page.dart
@@ -23,3 +23,95 @@ class RecordVideoPage extends StatelessWidget {
     );
   }
 }
+
+// class GalleryAccess extends StatefulWidget {
+//   const GalleryAccess({super.key});
+
+//   @override
+//   State<GalleryAccess> createState() => _GalleryAccessState();
+// }
+
+// class _GalleryAccessState extends State<GalleryAccess> {
+//   File? galleryFile;
+//   final picker = ImagePicker();
+//   @override
+//   Widget build(BuildContext context) {
+//     return Builder(
+//       builder: (BuildContext context) {
+//         return Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               ElevatedButton(
+//                 style: ElevatedButton.styleFrom(
+//                   backgroundColor: Colors.green,
+//                   foregroundColor: Colors.white,
+//                 ),
+//                 child: const Text('Select Image from Gallery and Camera'),
+//                 onPressed: () {
+//                   _showPicker(context: context);
+//                 },
+//               ),
+//               const SizedBox(height: 20),
+//               SizedBox(
+//                 height: 200.0,
+//                 width: 300.0,
+//                 child: galleryFile == null
+//                     ? const Center(child: Text('Sorry nothing selected!!'))
+//                     : Center(child: Image.file(galleryFile!)),
+//               ),
+//             ],
+//           ),
+//         );
+//       },
+//     );
+//   }
+
+//   void _showPicker({required BuildContext context}) {
+//     showModalBottomSheet(
+//       context: context,
+//       builder: (BuildContext context) {
+//         return SafeArea(
+//           child: Wrap(
+//             children: <Widget>[
+//               ListTile(
+//                 leading: const Icon(Icons.photo_library),
+//                 title: const Text('Photo Library'),
+//                 onTap: () {
+//                   getImage(ImageSource.gallery);
+//                   Navigator.of(context).pop();
+//                 },
+//               ),
+//               ListTile(
+//                 leading: const Icon(Icons.photo_camera),
+//                 title: const Text('Camera'),
+//                 onTap: () {
+//                   getImage(ImageSource.camera);
+//                   Navigator.of(context).pop();
+//                 },
+//               ),
+//             ],
+//           ),
+//         );
+//       },
+//     );
+//   }
+
+//   Future getImage(ImageSource img) async {
+//     // pick image from gallary
+//     final pickedFile = await picker.pickImage(source: img);
+//     // store it in a valid variable
+//     XFile? xfilePick = pickedFile;
+//     setState(() {
+//       if (xfilePick != null) {
+//         // store that in global variable galleryFile in the form of File
+//         galleryFile = File(pickedFile!.path);
+//       } else {
+//         ScaffoldMessenger.of(context).showSnackBar(
+//           // is this context <<<
+//           const SnackBar(content: Text('Nothing is selected')),
+//         );
+//       }
+//     });
+//   }
+// }
--- smile_authenticity_trainer/lib/upload_video_page.dart
+++ smile_authenticity_trainer/lib/upload_video_page.dart
@@ -4,6 +4,7 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
 
 import 'my_app_bar.dart';
 
@@ -15,14 +16,6 @@ class UploadVideoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // body: Card(
-      //   shadowColor: Colors.transparent,
-      //   margin: const EdgeInsets.all(8.0),
-      //   child: SizedBox.expand(
-      //     child: Center(child: Text('Upload video', style: theme.textTheme.titleLarge)),
-      //   ),
-      // ),
-      // body: GalleryAccess(),
       body: BlocProvider(
         create: (_) => UploadVideoCubit(),
         child: UploadVideoBody(),
@@ -44,7 +37,7 @@ class UploadVideoBody extends StatelessWidget {
           child: FilledButton(
             onPressed: () async {
               final picker = ImagePicker();
-              final pickedFile = await picker.pickImage(
+              final pickedFile = await picker.pickVideo(
                 source: ImageSource.gallery,
               );
 
@@ -75,106 +68,48 @@ class UploadVideoBody extends StatelessWidget {
   }
 }
 
-class PickingVideoBody extends StatelessWidget {
+class PickingVideoBody extends StatefulWidget {
   final File file;
 
   const PickingVideoBody(this.file, {super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Text('bbbbbbbbbbbbb');
+  State<PickingVideoBody> createState() => _PickingVideoBodyState();
+}
+
+class _PickingVideoBodyState extends State<PickingVideoBody> {
+  VideoPlayerController? _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = VideoPlayerController.file(widget.file);
+    _controller!.initialize();
+    _controller!.play();
   }
-}
 
-class GalleryAccess extends StatefulWidget {
-  const GalleryAccess({super.key});
-
-  @override
-  State<GalleryAccess> createState() => _GalleryAccessState();
-}
-
-class _GalleryAccessState extends State<GalleryAccess> {
-  File? galleryFile;
-  final picker = ImagePicker();
   @override
   Widget build(BuildContext context) {
-    return Builder(
-      builder: (BuildContext context) {
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.green,
-                  foregroundColor: Colors.white,
-                ),
-                child: const Text('Select Image from Gallery and Camera'),
-                onPressed: () {
-                  _showPicker(context: context);
-                },
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                height: 200.0,
-                width: 300.0,
-                child: galleryFile == null
-                    ? const Center(child: Text('Sorry nothing selected!!'))
-                    : Center(child: Image.file(galleryFile!)),
-              ),
-            ],
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: AspectRatio(
+            aspectRatio: _controller!.value.aspectRatio,
+            child: VideoPlayer(_controller!),
           ),
-        );
-      },
+        ),
+        Center(child: Text('Your tips')),
+      ],
     );
   }
 
-  void _showPicker({required BuildContext context}) {
-    showModalBottomSheet(
-      context: context,
-      builder: (BuildContext context) {
-        return SafeArea(
-          child: Wrap(
-            children: <Widget>[
-              ListTile(
-                leading: const Icon(Icons.photo_library),
-                title: const Text('Photo Library'),
-                onTap: () {
-                  getImage(ImageSource.gallery);
-                  Navigator.of(context).pop();
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.photo_camera),
-                title: const Text('Camera'),
-                onTap: () {
-                  getImage(ImageSource.camera);
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
+  @override
+  void dispose() {
+    super.dispose();
 
-  Future getImage(ImageSource img) async {
-    // pick image from gallary
-    final pickedFile = await picker.pickImage(source: img);
-    // store it in a valid variable
-    XFile? xfilePick = pickedFile;
-    setState(() {
-      if (xfilePick != null) {
-        // store that in global variable galleryFile in the form of File
-        galleryFile = File(pickedFile!.path);
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          // is this context <<<
-          const SnackBar(content: Text('Nothing is selected')),
-        );
-      }
-    });
+    _controller!.dispose();
   }
 }
 
