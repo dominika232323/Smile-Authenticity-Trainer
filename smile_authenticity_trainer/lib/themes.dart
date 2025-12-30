fe78bf4b686712ae3b0abde38a2103cc29d26c35
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2025-12-30 15:19:45 +0100

    Change surface color in light mode

--- smile_authenticity_trainer/lib/themes.dart
+++ smile_authenticity_trainer/lib/themes.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 ThemeData lightMode = ThemeData(
   brightness: Brightness.light,
   colorScheme: ColorScheme.light(
-    surface: Colors.grey.shade400,
+    surface: Colors.grey.shade100,
     primary: Colors.grey.shade300,
     secondary: Colors.grey.shade200,
     tertiary: Colors.amber,
