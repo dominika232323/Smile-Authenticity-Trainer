0095f2ad18a7d0164e57042d3f99efb958c4c9df
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2026-01-03 19:48:55 +0100

    Move calendar to MyCalendar class

--- smile_authenticity_trainer/lib/history.dart
+++ smile_authenticity_trainer/lib/history.dart
@@ -11,27 +11,29 @@ class HistoryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          TableCalendar(
-            headerStyle: HeaderStyle(
-              formatButtonVisible: false,
-              titleCentered: true,
-            ),
-            firstDay: DateTime.utc(2010, 10, 16),
-            lastDay: DateTime.utc(2030, 3, 14),
-            focusedDay: DateTime.now(),
-            startingDayOfWeek: StartingDayOfWeek.monday,
-            calendarStyle: CalendarStyle(
-              todayDecoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.tertiary,
-                shape: BoxShape.circle,
-              ),
-            ),
-          ),
-        ],
-      ),
+      body: Column(children: [MyCalendar()]),
       appBar: buildMyAppBar(context),
+    );
+  }
+}
+
+class MyCalendar extends StatelessWidget {
+  const MyCalendar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+      headerStyle: HeaderStyle(formatButtonVisible: false, titleCentered: true),
+      firstDay: DateTime.utc(2010, 10, 16),
+      lastDay: DateTime.utc(2030, 3, 14),
+      focusedDay: DateTime.now(),
+      startingDayOfWeek: StartingDayOfWeek.monday,
+      calendarStyle: CalendarStyle(
+        todayDecoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.tertiary,
+          shape: BoxShape.circle,
+        ),
+      ),
     );
   }
 }
