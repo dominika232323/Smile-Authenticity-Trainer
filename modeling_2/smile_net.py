eb7f7fff7bf6288435248e2907772567a734fb19
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2026-01-09 04:36:40 +0100

    Create new training pipeline

---
+++ modeling_2/smile_net.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+
+
+class SmileNet(nn.Module):
+    def __init__(self, input_dim, dropout_p: float = 0.3):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, 128),
+            nn.ReLU(),
+            nn.Dropout(dropout_p),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Dropout(dropout_p),
+            nn.Linear(64, 1)
+        )
+
+    def forward(self, x):
+        return self.net(x).squeeze(1)
