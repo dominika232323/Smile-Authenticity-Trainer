0ef8b4c734a132be1d2e33b9c358724d8577d924
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2026-01-10 18:44:31 +0100

    Start hyperparameter search for eyes features

--- modeling/eyes_features.py
+++ modeling/eyes_features.py
@@ -14,14 +14,21 @@ def main():
     non_feature_cols = ["filename"]
 
     param_grid = {
-        "batch_size": [32],
-        "dropout": [0.3],
+        "batch_size": [32, 64],
+        "dropout": [0.1, 0.3, 0.5],
         "epochs": [50],
         "patience": [7],
-        "lr": [1e-3],
+        "lr": [3e-4, 1e-3],
         "test_size": [0.2],
-        "how_many_features": [50],
-        "threshold": [0.5],
+        "how_many_features": [30, 50, 75],
+        "threshold": [0.4, 0.5, 0.6],
+        "hidden_dims": [
+            [],
+            [64],
+            [128],
+            [128, 64],
+            [256, 128],
+        ],
     }
     hyperparameter_grid_search(dataset_path, EYES_RUNS_DIR, param_grid, non_feature_cols)
 
