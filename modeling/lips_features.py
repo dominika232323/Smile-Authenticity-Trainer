3185776e1605c635237b8e9ea73b89105355754c
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2026-01-10 13:34:42 +0100

    Start hyperparameter search for lips features

--- modeling/lips_features.py
+++ modeling/lips_features.py
@@ -14,14 +14,14 @@ def main():
     non_feature_cols = ["filename"]
 
     param_grid = {
-        "batch_size": [32],
-        "dropout": [0.3],
-        "epochs": [50],
-        "patience": [7],
-        "lr": [1e-3],
-        "test_size": [0.2],
-        "how_many_features": [50],
-        "threshold": [0.5],
+        "batch_size": [8, 16, 32, 64],
+        "dropout": [0.2, 0.3, 0.4, 0.5],
+        "epochs": [150],
+        "patience": [3, 5, 7, 10],
+        "lr": [1e-2, 1e-3, 1e-4, 1e-5],
+        "test_size": [0.2, 0.25, 0.3, 0.4],
+        "how_many_features": [25, 40, 50, 70],
+        "threshold": [0.4, 0.5, 0.6, 0.7],
     }
     hyperparameter_grid_search(dataset_path, LIPS_RUNS_DIR, param_grid, non_feature_cols)
 
